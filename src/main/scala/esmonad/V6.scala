f0da1c933cd6527518550d51b406964a2435ef70
Étienne Vallette d'Osia <etienne.vallette-dosia@fabernovel.com>
2017-10-25 15:19:53 +0200

    Make V6 the monad version

--- src/main/scala/esmonad/V6.scala
+++ src/main/scala/esmonad/V6.scala
@@ -1,33 +1,66 @@
 package esmonad
 
-trait V6Sourced { // Better syntax
+import cats.{FlatMap, Functor}
+import cats.instances.either._
+import cats.syntax.functor._
+import cats.syntax.flatMap._
 
-  case class Sourced[STATE, EVENT](
-    handler: (Option[STATE], EVENT) => Some[STATE],
-    value: Either[String, (Seq[EVENT], STATE)],
-  ) {
-    def events = value.map { case (events, _) => events }
-    def and(fn: STATE => Either[String, EVENT]): Sourced[STATE, EVENT] = {
-      Sourced(handler, value.flatMap { case (events, state) =>
-        fn(state).map { newEvent =>
-          val Some(newState) = handler(Some(state), newEvent)
-          (events :+ newEvent, newState)
+trait V6Sourced { self: V4Handler =>
+
+  case class SourcedT[F[_], STATE, EVENT](run: F[(Vector[EVENT], STATE)]) {
+
+    def events(implicit F: Functor[F]): F[Vector[EVENT]] = run.map { case (events, _) => events }
+
+    def map[B](f: STATE => B)(implicit F: Functor[F]): SourcedT[F, B, EVENT] = {
+      SourcedT[F, B, EVENT](run.map { case (events, state) =>
+        (events, f(state))
+      })
+    }
+
+    def flatMap[B](f: STATE => SourcedT[F, B, EVENT])(implicit F: FlatMap[F]): SourcedT[F, B, EVENT] = {
+      SourcedT(run.flatMap { case (oldEvents, oldState) =>
+        f(oldState).run.map { case (newEVents, newState) =>
+          (oldEvents ++ newEVents, newState)
         }
       })
     }
+
   }
+
+  //type SourcedT[F[_], STATE, EVENT] = WriterT[F, Vector[EVENT], STATE]
+
+  type Sourced[STATE, EVENT] = SourcedT[Either[String, ?], STATE, EVENT]
+
   object Sourced {
-    def empty[STATE, EVENT](
-      handler: (Option[STATE], EVENT) => Some[STATE],
-      value: Either[String, EVENT]
-    ): Sourced[STATE, EVENT] = {
-      Sourced(handler, value.map { event =>
-        val Some(state) = handler(None, event)
-        (Seq(event), state)
+
+    def source[STATE, EVENT](
+      block: STATE => Either[String, EVENT]
+    )(implicit handler: EventHandler[STATE, EVENT]): STATE => Sourced[STATE, EVENT] =
+      state => new Sourced(block(state).map { e =>
+        val Some(newState) = handler(Some(state), e)
+        Vector(e) -> newState
       })
+
+    def source[STATE, EVENT](
+      state: STATE,
+      block: STATE => Either[String, EVENT]
+    )(implicit handler: EventHandler[STATE, EVENT]): Sourced[STATE, EVENT] =
+      new Sourced(block(state).map { e =>
+        val Some(newState) = handler(Some(state), e)
+        Vector(e) -> newState
+      })
+
+    final class SourceNewPartiallyApplied[STATE] {
+      def apply[EVENT](block: Either[String, EVENT])(implicit handler: EventHandler[STATE, EVENT]): Sourced[STATE, EVENT] =
+        new Sourced(block.map { e =>
+          val Some(newState) = handler(None, e)
+          Vector(e) -> newState
+        })
     }
+
+    def sourceNew[STATE]: SourceNewPartiallyApplied[STATE] = new SourceNewPartiallyApplied[STATE]
   }
 
 }
 
-object V6 extends V6Sourced with V4Handler with V5Journal with V5Models
+object V6 extends V6Sourced with V5Models with V4Handler with V5Journal
--- src/test/scala/esmonad/V6Spec.scala
+++ src/test/scala/esmonad/V6Spec.scala
@@ -1,36 +1,45 @@
 package esmonad
 
-import scala.concurrent.Future
-
 import cats.data.{EitherT, OptionT}
+import cats.instances.either._
 import cats.instances.future._
-import org.scalatest._
+import org.scalatest.{AsyncFlatSpec, Matchers}
+import scala.concurrent.Future
 
 class V6Spec extends AsyncFlatSpec with Matchers {
   import esmonad.V6._
+  import Sourced._
 
   "The V6 object" should "be valid" in {
 
-    (
+    def walkRight(dist: Int)(state: Turtle) =
       for {
-        events <- EitherT.fromEither(
-          Sourced.empty[Turtle, TurtleEvent](
-            Turtle.handler,
-            Turtle.create("123", Position.zero, North)
-          ) and
-          Turtle.walk(1) and
-          Turtle.turn(ToRight) and
-          Turtle.walk(1) and
-          Turtle.turn(ToRight) and
-          Turtle.walk(2) and
-          Turtle.turn(ToRight) and
-          Turtle.walk(2) events
+        state1 <- source(state, Turtle.walk(dist))
+        state2 <- source(state1, Turtle.turn(ToRight))
+      } yield state2
+
+    {
+      for {
+        events <- EitherT.fromEither[Future](
+          sourceNew[Turtle](Turtle.create("123", Position.zero, North))
+            .flatMap(walkRight(1))
+            .flatMap(walkRight(1))
+            .flatMap(source(Turtle.walk(2)))
+            .events
         )
         _ <- EitherT.right(persist(events))
 
+        state1 <- OptionT(hydrate[Turtle]("123")).toRight("not found")
+        moreEvents <- EitherT.fromEither[Future](
+          source(state1, Turtle.turn(ToRight))
+            .flatMap(source(Turtle.walk(2)))
+            .events
+        )
+        _ <- EitherT.right(persist(moreEvents))
+
         state3 <- OptionT(hydrate[Turtle]("123")).toRight("not found")
       } yield state3
-    ).value.map {
+    }.value.map {
       _ shouldBe Right(Turtle("123", Position(-1, -1), West))
     }
 

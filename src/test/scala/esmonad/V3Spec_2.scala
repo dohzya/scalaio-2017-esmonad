e9765f46e89e033fb733bc9de35d8cb8303b2ea4
Étienne Vallette d'Osia <etienne.vallette-dosia@fabernovel.com>
2017-10-27 10:52:14 +0200

    simplify V3Spec_2 syntax

--- src/test/scala/esmonad/V3Spec_2.scala
+++ src/test/scala/esmonad/V3Spec_2.scala
@@ -15,14 +15,14 @@ class V3Spec_2 extends AsyncFlatSpec with Matchers {
         _ <- EitherT.right(persist(events))
         state <- OptionT(hydrate[Turtle]("123")).toRight("not found")
         moreEvents <- EitherT.fromEither(many[Turtle, TurtleEvent](state, Seq(
-          Turtle.walk(1)(_),
-          Turtle.turn(ToRight)(_),
-          Turtle.walk(1)(_),
-          Turtle.turn(ToRight)(_),
-          Turtle.walk(2)(_),
-          Turtle.turn(ToRight)(_),
-          Turtle.walk(2)(_),
-        ))(Turtle.handler).map { case (events, _) => events })
+          Turtle.walk(1),
+          Turtle.turn(ToRight),
+          Turtle.walk(1),
+          Turtle.turn(ToRight),
+          Turtle.walk(2),
+          Turtle.turn(ToRight),
+          Turtle.walk(2),
+        ))(Turtle.handler).map { case (evts, _) => evts })
         _ <- EitherT.right(persist(moreEvents))
 
         state3 <- OptionT(hydrate[Turtle]("123")).toRight("not found")

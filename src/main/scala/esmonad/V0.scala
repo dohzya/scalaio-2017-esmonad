238449fde2e0d1389537322a2fea408ff255240e
Daniel Krzywicki <daniel.krzywicki@fabernovel.com>
2017-10-27 15:04:36 +0200

    add validation on turtle creation

--- src/main/scala/esmonad/V0.scala
+++ src/main/scala/esmonad/V0.scala
@@ -6,8 +6,10 @@ trait V0Models {
 
   object Turtle {
 
-    def create(id: String, pos: Position, dir: Direction): Either[String, Turtle] =
-      Right(Turtle(id, pos, dir))
+    def create(id: String, pos: Position, dir: Direction): Either[String, Turtle] = {
+      if (pos.x.abs > 100 || pos.y.abs > 100) Left("Too far away")
+      else Right(Turtle(id, pos, dir))
+    }
 
     def turn(rot: Rotation)(turtle: Turtle): Either[String, Turtle] =
       Right(turtle.copy(dir = turtle.dir.rotate(rot)))
